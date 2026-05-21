95d11f6ac235c6d9b9717b70346e4033df15efad
서민재 <hey_minj@naver.com>
2026-05-21 23:38:17 +0900

    feat : 회원가입, 로그인 기능 추가

---
+++ accounts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField(label="이메일", required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "email")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("이미 사용 중인 이메일입니다.")
+
+        return email
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
+from accounts.forms import UserForm
 
-# Create your views here.
 def index(request):
     return HttpResponse("안녕하세요. 오신것을 환영합니다.")
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('accounts:index')
+    else:
+        form = UserForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+        if user is not None:
+            login(request, user)
+            return redirect('accounts:index')
+        else:
+            return render(request, 'accounts/login.html', {
+                'error': '아이디 또는 비밀번호가 올바르지 않습니다.'
+            })
+    return render(request, 'accounts/login.html')
